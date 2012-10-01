b1898c3346e7ab95ef6e0355ebc5ff80dc7e5c9d
declantyson <declantyson@hotmail.co.uk>
2012-10-01 16:04:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+The Great British Dangerzone
+=========================
+
+* Rails are dangerous
+* So is ruby mining
+* So is East London
+
+## Frankly I'm in a fairly precarious situation
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreatBritishDangerzone::Application.config.secret_token = '9d9f5f601c5f358aed93fa215f2076c343b57def9c7ab37faaa1703cfd3b529cb00d2fc099849821cbfef57d4dac5ab84aae54fc229569e63196d79b8c78cf1a'
