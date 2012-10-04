e53ce9874b1877875f0cf6371805f62089a92dfa
declantyson <declantyson@hotmail.co.uk>
2012-10-04 16:10:14 +0100

    added css styles and templates

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,33 +2,21 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'rail me ruby'" do
-      visit '/static_pages/home'
-      page.should have_content('rail me ruby')
-    end
     it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Roobn00b | home')
+      visit home_path
+      page.should have_selector('title', :text => 'StoryStore | Home')
     end
   end
   describe "Help page" do
-    it "should have the content 'Problem?'" do
-      visit '/static_pages/help'
-      page.should have_content('Problem?')
-    end
     it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => 'Roobn00b | help')
+      visit help_path
+      page.should have_selector('title', :text => 'StoryStore | Help')
     end
   end
   describe "About page" do
-    it "should have the content 'What are you doing here?'" do
-      visit '/static_pages/about'
-      page.should have_content('What are you doing here?')
-    end
     it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => 'Roobn00b | aboot')
+      visit about_path
+      page.should have_selector('title', :text => 'StoryStore | About')
     end
   end
 end
