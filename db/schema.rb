c599b031585ab349de8b1f4ef637711d8515526c
declantyson <declantyson@hotmail.co.uk>
2012-10-08 15:13:16 +0100

    added user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121003154127) do
+ActiveRecord::Schema.define(:version => 20121008122416) do
 
   create_table "characters", :force => true do |t|
     t.string   "name"
@@ -94,8 +94,9 @@ ActiveRecord::Schema.define(:version => 20121003154127) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
