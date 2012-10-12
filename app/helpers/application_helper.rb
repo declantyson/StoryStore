8cdd5fd1ac016f01db66ae0f26afb7ffdd574e83
declantyson <declantyson@hotmail.co.uk>
2012-10-12 13:51:32 +0100

    added format dropdown and fixed some shitty css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,20 @@ module ApplicationHelper
     genres["nf"] = "Non-fiction/Biographical"
     genres["scifi"] = "Sci Fi/Fantasy"
     genres["thriller"] = "Thriller"
+    genres["other"] = "Other"
     genres
+  end
+
+  # list of formats
+  def format_list()
+    formats = {}
+    formats["book"] = "Book"
+    formats["animation"] = "Animation"
+    formats["film"] = "Film"
+    formats["play"] = "Play"
+    formats["game"] = "Game"
+    formats["other"] = "Other"
+    formats
   end
 
   # Returns the full title on a per-page basis.
@@ -25,8 +38,14 @@ module ApplicationHelper
     end
   end
 
+  # Generate a dropdown form based on the genre_list()
   def get_genre_dropdown()
     collection_select("project", "genres", genre_list(), :first, :last, {:prompt => "Please select"})
+  end
+
+  # Generate a dropdown form based on the format_list()
+  def get_format_dropdown()
+    collection_select("project", "format", format_list(), :first, :last, {:prompt => "Please select"})
   end
 
 end
