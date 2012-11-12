4ac53d64230566ff045b5580853032742fe4cb3c
declantyson <declantyson@hotmail.co.uk>
2012-11-12 16:36:49 +0000

    made the homepage a bit more interesting, closes #6

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,10 @@ class StaticPagesController < ApplicationController
   def home
   	@projects = Project.all
   	@projects.sort!{|a, b| b.average_feedback_rating <=> a.average_feedback_rating}
+
+    @recentprojects = Project.order("created_at desc").limit(10)
+
+    @inspiration = Inspiration.order("RANDOM()").limit(10)
   end
 
   def about
