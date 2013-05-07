985cceda2e9c5f191235d4554fbf908ddf074e30
declantyson <declantyson@hotmail.co.uk>
2013-05-07 15:22:21 +0100

    oop, i accidentally a number

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
   	@projects = Project.all
   	@projects.sort!{|a, b| b.average_feedback_rating <=> a.average_feedback_rating}
 
-    @recentprojects = Project.where(:privacy => false).order("created_at desc").limit(1)
+    @recentprojects = Project.where(:privacy => false).order("created_at desc").limit(10)
     @inspiration = Inspiration.joins(:project).where("projects.privacy = ?", false).order("RANDOM()").limit(10)
   end
 
